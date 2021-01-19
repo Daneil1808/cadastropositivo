6d22cb578a0f97883c9d40724f35db99f941d215
Daneil1808 <daniel.goncalves2@estudante.ufla.br>
2021-01-18 21:07:47 -0300

    primeiro commit

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/pecuarista',
+    name: 'Pecuarista',
+    component: () => import('../views/Pecuarista.vue')
+  },
+  {
+    path: '/frigorifico',
+    name: 'Frigorifico',
+    component: () => import('../views/Frigorifico.vue')
+  },
+  {
+    path: '/administrador',
+    name: 'Administrador',
+    component: () => import('../views/Administrador.vue')
+  }
+]
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+export default router
